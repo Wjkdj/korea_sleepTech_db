9e76390a918c1b9fd20c90ae5ae9167d19b31523
Wjkdj <wlsdnxo0807@gmail.com>
2025-04-16 17:29:44 +0900

    20250416 JWT

--- k_트랜잭션/a_트랜잭션.sql
+++ k_트랜잭션/a_트랜잭션.sql
@@ -1,12 +1,13 @@
-### 트랜잭션(transaction - 처리) ###
+### 'k_트랜잭션' 폴더 >>> 'a_트랜잭션' 파일 ###
+
+### 1. 트랜잭션(transaction - 처리) ###
 # : DB에서 하나의 논리적인 작업 단위를 의미
 # : 여러 개의 작업(명령어)이 한 번에 처리되어야 할 때 사용
-# - 중간에 하나라도 실패하면 '전체 작업을 취소(Rollbacak)'
+# - 중간에 하나라도 실패하면 '전체 작업을 취소(Rollback)'
 # - 모든 작업이 '문제없이 완료되면 확정(Commit)'
-
 /*
 	은행에서 A가 B에게 돈을 보낼 때
-    A 계좌에서 출금 + B 계좌에 입금
+    A 계좌에서 출금 + B 계좌에 입금하는 두 작업은 반드시! 함께 처리
     >> 하나만 성공하고 하나는 실패하면 돈이 사라지거나 두 번 생기는 문제 발생
 */
 
@@ -21,7 +22,7 @@
 # 2) SQL 실행(작업 실행): 여러 SQL 명령어를 통해 트랜잭션 내에서 실행(INSERT, UPDATE 등)
 # 3) 성공하면 커밋: 모든 작업이 성공적으로 완료되면, DB에 변경 사항을 확정(COMMIT)
 # 4) 실패하면 롤백: 작업 중 오류 발생 | 작업을 취소하고 싶을 때(ROLLBACK)
-#	>> 트랜잭션 시작 전 상태로 되돌림
+#    >> 트랜잭션 시작 전 상태로 되돌림
 
 /*
 	start transaction;
@@ -31,7 +32,7 @@
     update account set balance = balance - 1000 where account_id = 'A';
     update account set balance = balance + 1000 where account_id = 'B';
     
-    commit;
+	commit;
     
     cf) savepoint: 트랜잭션 내에서 특정 지점을 저장하여 해당 지점으로 롤백
 		savepoint 이름
@@ -58,19 +59,95 @@ create table `purchases` (
     foreign key (member_id) references members(member_id)
 );
 
+select @@autocommit; # 1: 자동 커밋이 활성화된 상태
+# >> 트랜잭션이 자동으로 commit (무조건 성공)
+
+set autocommit = 0; # 트랜잭션 수동 처리
+
 -- 트랜잭션 시작
 start transaction;
 
 # 이후 SQL문은 하나의 명령문으로 처리
-insert into members
+insert into members 
 values
-	(1, '이승아', 30);
+	(3, '이승아', 30);
     
 insert into purchases
 values
-	(1, 1, '노트북', 200);
-    
+	(3, 2, '노트북', 200); # 외래키 제약 조건 오류
+
 commit; # 예외가 없으면 변경 사항을 저장
 
 select * from members;
 select * from purchases;
+
+###
+create table `accounts` (
+	account_id int primary key auto_increment,
+    account_holder varchar(50),
+    balance int unsigned
+);
+
+create table transaction_log (
+	transaction_in int primary key auto_increment,
+    from_account_id int,
+    to_account_id int,
+    amount int,
+    transaction_date datetime default current_timestamp,
+    foreign key (from_account_id) references accounts(account_id),
+    foreign key (to_account_id) references accounts(account_id)
+);
+
+insert into accounts (account_holder, balance)
+values
+	('이승아', 5000),
+	('이도경', 3000);
+
+### 스토어드 프로시저 작성: SQL 내의 조건문이나 반복문은 SQL 스크립트 내에서 바로 실행X, 프로시저나 트리거 내에서 사용 ###
+delimiter $$
+
+create procedure tansfer_money()
+begin
+	declare from_balance int; -- 변수 선언
+    
+	-- 자동 커밋 해제
+	set autocommit = 0;
+
+	start transaction; 
+
+	select balance into from_balance # select 결과를 변수에 저장할 때 사용
+	from accounts
+	where account_holder = '이승아';
+
+	-- 잔액이 1000원 이상일 경우에만 송금 실행
+	if from_balance >= 1000 then
+		
+		update accounts
+		set balance = balance - 1000
+		where account_holder = '이승아';
+
+		update accounts
+		set balance = balance + 1000
+		where account_holder = '이도경';
+
+		insert into transaction_log (from_account_id, to_account_id, amount)
+		values (
+			(select account_id from accounts where account_holder = '이승아'),
+			(select account_id from accounts where account_holder = '이도경'),
+			1000
+		);
+
+		commit;
+	else
+		rollback;
+
+	end if;
+
+	set autocommit = 1;
+end $$
+
+delimiter ;
+
+call tansfer_money();
+
+select * from transaction_log;
